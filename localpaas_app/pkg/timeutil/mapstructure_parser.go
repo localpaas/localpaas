f111dbcf497afd385594ef2a189316e90b22ed39
tiendc <tiendc@gmail.com>
2026-03-16 03:23:59 +0700

    Add supports for timeutil.Duration in mapstructure hook funcs

--- localpaas_app/pkg/timeutil/mapstructure_parser.go
+++ localpaas_app/pkg/timeutil/mapstructure_parser.go
@@ -7,43 +7,59 @@ import (
 	"github.com/mitchellh/mapstructure"
 )
 
+var (
+	typeDate        = reflect.TypeFor[Date]()
+	typeStdTime     = reflect.TypeFor[time.Time]()
+	typeDuration    = reflect.TypeFor[Duration]()
+	typeStdDuration = reflect.TypeFor[time.Duration]()
+
+	// TODO: support pointer types such as *time.Time, etc
+)
+
 // MapstructureParseDateFunc custom decoder hook for mapstructure
+//
+//nolint:forcetypeassert
 func MapstructureParseDateFunc() mapstructure.DecodeHookFunc {
 	return func(f reflect.Type, t reflect.Type, data any) (any, error) {
-		if t != reflect.TypeFor[Date]() {
+		if t != typeDate {
 			return data, nil
 		}
 		switch f.Kind() { //nolint:exhaustive
 		case reflect.String:
-			return ParseDate(data.(string)) //nolint:forcetypeassert
+			return ParseDate(data.(string))
 		default:
 			return data, nil
 		}
 	}
 }
 
+//nolint:forcetypeassert
 func MapstructureParseTimeFunc() mapstructure.DecodeHookFunc {
 	return func(f reflect.Type, t reflect.Type, data any) (any, error) {
-		if t != reflect.TypeFor[time.Time]() {
+		if t != typeStdTime {
 			return data, nil
 		}
 		switch f.Kind() { //nolint:exhaustive
 		case reflect.String:
-			return time.Parse(time.RFC3339, data.(string)) //nolint:forcetypeassert
+			return time.Parse(time.RFC3339, data.(string))
 		default:
 			return data, nil
 		}
 	}
 }
 
+//nolint:forcetypeassert
 func MapstructureParseDurationFunc() mapstructure.DecodeHookFunc {
 	return func(f reflect.Type, t reflect.Type, data any) (any, error) {
-		if t != reflect.TypeFor[time.Duration]() {
-			return data, nil
-		}
 		switch f.Kind() { //nolint:exhaustive
 		case reflect.String:
-			return time.ParseDuration(data.(string)) //nolint:forcetypeassert
+			if t == typeDuration {
+				return ParseDuration(data.(string))
+			}
+			if t == typeStdDuration {
+				return time.ParseDuration(data.(string))
+			}
+			return data, nil
 		default:
 			return data, nil
 		}
